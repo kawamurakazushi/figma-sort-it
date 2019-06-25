e4fa4593bffdb2da704bd032df3cbe65bdf473b5
Kazushi Kawamura <wakaka2277@gmail.com>
2019-06-25 15:09:13 +0900

    feat: use sub menu instead of ui

--- code.ts
+++ code.ts
@@ -1,42 +1,35 @@
-figma.showUI(__html__, { height: 136 });
+// Some Big number
+let startingIndex = 100000;
 
-figma.ui.onmessage = msg => {
-  if (msg.type === "sort") {
-    // Some Big number
-    let startingIndex = 100000;
+const sorted = figma.currentPage.selection
+  .map(node => {
+    const parent = node.parent;
+    startingIndex = Math.min(startingIndex, parent.children.indexOf(node));
 
-    const sorted = figma.currentPage.selection
-      .map(node => {
-        const parent = node.parent;
-        startingIndex = Math.min(startingIndex, parent.children.indexOf(node));
-
-        const clone = node.clone();
-        return {
-          node: clone,
-          parent
-        };
-      })
-      .sort((a, b) => {
-        if (msg.order === "desc") {
-          return a.node.name.localeCompare(b.node.name, undefined, {
-            numeric: true
-          });
-        }
-        return b.node.name.localeCompare(a.node.name, undefined, {
-          numeric: true
-        });
+    return {
+      node: node.clone(),
+      parent
+    };
+  })
+  .sort((a, b) => {
+    if (figma.command === "desc") {
+      return a.node.name.localeCompare(b.node.name, undefined, {
+        numeric: true
       });
-
-    // Insert sorted nodes
-    sorted.forEach((obj, i) => {
-      obj.parent.insertChild(startingIndex + i, obj.node);
+    }
+    return b.node.name.localeCompare(a.node.name, undefined, {
+      numeric: true
     });
+  });
 
-    // Remove old nodes
-    figma.currentPage.selection.forEach(node => {
-      node.remove();
-    });
-  }
+// Insert sorted nodes
+sorted.forEach((obj, i) => {
+  obj.parent.insertChild(startingIndex + i, obj.node);
+});
 
-  figma.closePlugin();
-};
+// Remove old nodes
+figma.currentPage.selection.forEach(node => {
+  node.remove();
+});
+
+figma.closePlugin();
